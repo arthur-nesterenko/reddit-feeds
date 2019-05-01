0840c45c93e27ad6305407450ea4e72bf2d7f7c2
arthurnesterenko <nesterenko19@gmail.com>
2019-05-01 20:37:31 +0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, Fragment } from 'react';
+import { Normalize } from '@smooth-ui/core-sc';
+import { ThemeProvider } from 'styled-components';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import screens from './screens';
+import theme from './assets/theme';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Normalize />
+        <BrowserRouter>
+          <Suspense fallback="Loading">
+            <Switch>
+              {screens.map((screen, index) => (
+                <Route key={index} {...screen} />
+              ))}
+            </Switch>
+          </Suspense>
+        </BrowserRouter>
+      </Fragment>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
